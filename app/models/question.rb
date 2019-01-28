525e8b850ec7bccbf6b7af5e7fdd8e4f3455d89a
Hidekazu_Moriguchi <23137382+Tachikomaps9@users.noreply.github.com>
2019-01-28 20:43:21 +0900

    回答一覧表示の実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   # has_many : モデル名 questionモデルは複数のanswerを持つという事になる
   # deppendent: :destroy とあるQuestionモデルが削除されたら紐付くanswerも削除される
-  has_many :answer, dependent: :destroy
+  has_many :answers, dependent: :destroy
   
   validates :name, presence: true
   validates :title, presence: true
